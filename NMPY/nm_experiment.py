4aba46efde7dc3d816ea1f878131070aacfd18a8
jasonsethrothman <jason@thinkrandom.com>
2019-09-27 18:27:58 +0100

    Testing h5py

--- NMPY/nm_experiment.py
+++ NMPY/nm_experiment.py
@@ -220,13 +220,29 @@ class Experiment(object):
         return False
 
     def folder_open_hdf5(self):
+        wave_prefix = "Record"
         with h5py.File('nmFolder0.hdf5', 'r') as f:
-            print(f.keys())
+            #print(f.keys())
+            for k in f.keys():
+                if k[0:len(wave_prefix)] == wave_prefix:
+                    print(k)
             # for name in f:
                 # print(name)
-            data = f['RecordA0']
-            print(data)
-            print(min(data))
-            pf = f['NMPrefix_Record']
-            print(pf)
+            d = f['RecordA0']
+            for i in d.attrs.keys():
+                print(i)
+            # cannot get access to attribute values for keys:
+            # probably need to update h5py to v 2.10
+            #IGORWaveNote
+            #IGORWaveType
+            #print(d.attrs.__getitem__('IGORWaveNote'))
+            #for a in d.attrs:
+                #print(item + ":", d.attrs[item])
+                #print(item + ":", d.attrs.get(item))
+                #print(a.shape)
+            #for k in a.keys():
+                #print(k)
+            #print(a)
+            #pf = f['NMPrefix_Record']
+            #print(pf)
 
